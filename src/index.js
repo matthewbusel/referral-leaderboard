20e2d94e07d46a1b0b72abe05d9f1aa6deefaae6
Glitch (referral-leaderboard-slash-to-modal) <none>
2020-01-11 21:17:32 +0000

    💶💾 Updated with Glitch 1/11/20

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const qs = require("querystring");
 const confirmationmsg = require("./confirmationmsg");
 const signature = require("./verifySignature");
 const debug = require("debug")("slash-command-template:index");
+const moment = require("moment");
 
 const apiUrl = "https://slack.com/api";
 
@@ -52,7 +53,7 @@ app.get("/", (req, res) => {
 
 // --------------------------------------------------------------------------------------------------------------------------
 // OAUTH
-// Uses OAuth2 standard to get a token upon 
+// Uses OAuth2 standard to get a token upon
 
 app.get("/auth", function(req, res) {
   if (!req.query.code) {
@@ -75,21 +76,23 @@ app.get("/auth", function(req, res) {
     if (!error && response.statusCode == 200) {
       // Get an auth token (and store the team_id / token)
       // storage.setItemSync(JSON.parse(body).team_id, JSON.parse(body).access_token);
+
       let team_id = JSON.parse(body).team.id;
       let access_token = JSON.parse(body).access_token;
-      
+
       // Search for existing team_id / access_token in db, update if team_id exists, create if team_id does not exist
       searchCreateToken(team_id, access_token);
-      
+
       // create a team object to save in DB in order to use the webhook channel and url for zapier posts
       const team = {};
       team.team_id = JSON.parse(body).team.id;
       team.channel = JSON.parse(body).incoming_webhook.channel;
       team.url = JSON.parse(body).incoming_webhook.url;
       team.name = JSON.parse(body).team.name;
-      
+      team.authed_user = JSON.parse(body).authed_user.id;
+
       searchCreateTeam(team);
-      
+
       res.sendStatus(200);
 
       // Show a nicer web page or redirect to Slack, instead of just giving 200 in reality!
@@ -166,7 +169,7 @@ const updateToken = (team_id, access_token, recordId) => {
         return;
       }
       records.forEach(function(record) {
-        console.log('Updated token for: ', record.get("team_id"));
+        console.log("Updated token for: ", record.get("team_id"));
       });
     }
   );
@@ -236,7 +239,8 @@ const createTeam = team => {
           TeamName: team.name,
           TeamId: team.team_id,
           webhook_channel: team.channel,
-          webhook_url: team.url
+          webhook_url: team.url,
+          authed_user: team.authed_user
         }
       }
     ],
@@ -260,7 +264,8 @@ const updateTeam = (team, recordId) => {
         id: recordId,
         fields: {
           webhook_channel: team.channel,
-          webhook_url: team.url
+          webhook_url: team.url,
+          authed_user: team.authed_user
         }
       }
     ],
@@ -270,143 +275,147 @@ const updateTeam = (team, recordId) => {
         return;
       }
       records.forEach(function(record) {
-        console.log('Updated webhooks for team: ', record.get("TeamId"));
+        console.log("Updated webhooks for team: ", record.get("TeamId"));
       });
     }
   );
 };
-
 
 // --------------------------------------------------------------------------------------------------------------------------
 // Endpoint to receive /referral slash command from Slack
 // Checks verification token and opens a dialog to capture more info
 
 app.post("/command", (req, res) => {
+  console.log(req.body);
 
   // extract the slash command text, and trigger ID from payload
-  const { text, trigger_id, user_name, team_id } = req.body;
-  
+  const { text, trigger_id, user_name, team_id, user_id } = req.body;
+
   // Verify the signing secret
   if (signature.isVerified(req)) {
     // create the dialog payload - includes the dialog structure, Slack API token,
     // and trigger ID
-    
+
     // retrieve the access token of the team who sent the /command post request to use to open a modal in their workspace
-    getToken(team_id, req, res)
-    .then(record => openView(record))
-    
+    getToken(team_id, req, res).then(record => openView(record));
+
     const openView = record => {
-    let access_token = record.access_token;
-    
-    const view = {
-      // token: process.env.SLACK_ACCESS_TOKEN,
-      token: access_token,
-      trigger_id,
-      view: JSON.stringify({
-        type: "modal",
-        title: {
-          type: "plain_text",
-          text: "Submit a referral!"
-        },
-        callback_id: "submit-referral",
-        submit: {
-          type: "plain_text",
-          text: "Submit"
-        },
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "plain_text",
-              text: `:wave: Hey ${user_name}!\n\nReady to move up the leaderboard and supercharge your team? Tell your hiring manager who they should speak with and earn points!`,
-              emoji: true
-            }
+      let access_token = record.access_token;
+
+      const view = {
+        // token: process.env.SLACK_ACCESS_TOKEN,
+        token: access_token,
+        trigger_id,
+        view: JSON.stringify({
+          type: "modal",
+          title: {
+            type: "plain_text",
+            text: "Submit a referral!"
           },
-          {
-            type: "divider"
+          callback_id: "submit-referral",
+          submit: {
+            type: "plain_text",
+            text: "Submit"
           },
-          {
-            block_id: "name_block",
-            type: "input",
-            label: {
-              type: "plain_text",
-              text: "Name"
+          blocks: [
+            {
+              type: "section",
+              // text: {
+              //   type: "mrkdwn",
+              //   text: ":wave: Hey <@" + user_id + ">, \n\nReady to move up the leaderboard? Tell your hiring manager who they should speak with and *earn 1 point* (whether they are hired or not)!"
+              // }
+              text: {
+                type: "mrkdwn",
+                text:
+                  ":wave: Hey <@" +
+                  user_id +
+                  ">, \n\nEarn *1 point* on the leaderboard for every referral you submit!"
+              }
             },
-            element: {
-              action_id: "name",
-              type: "plain_text_input"
+            {
+              type: "divider"
             },
-            hint: {
-              type: "plain_text",
-              text: "Who should we speak to?"
+            {
+              block_id: "name_block",
+              type: "input",
+              label: {
+                type: "plain_text",
+                text: "Name"
+              },
+              element: {
+                action_id: "name",
+                type: "plain_text_input"
+              },
+              hint: {
+                type: "plain_text",
+                text: "Who should we speak with?"
+              }
+            },
+            {
+              block_id: "position_block",
+              type: "input",
+              label: {
+                type: "plain_text",
+                text: "Position"
+              },
+              element: {
+                action_id: "position",
+                type: "plain_text_input"
+              },
+              hint: {
+                type: "plain_text",
+                text: "What role would might they have?"
+              }
+            },
+            {
+              block_id: "email_block",
+              type: "input",
+              label: {
+                type: "plain_text",
+                text: "Email"
+              },
+              element: {
+                action_id: "email",
+                type: "plain_text_input"
+              },
+              hint: {
+                type: "plain_text",
+                text: "How can we contact them?"
+              }
+            },
+            {
+              block_id: "linkedin_block",
+              type: "input",
+              label: {
+                type: "plain_text",
+                text: "LinkedIn"
+              },
+              element: {
+                action_id: "linkedin",
+                type: "plain_text_input"
+              },
+              hint: {
+                type: "plain_text",
+                text: "Could you share their LinkedIn profile?"
+              }
+              // optional: true
             }
-          },
-          {
-            block_id: "position_block",
-            type: "input",
-            label: {
-              type: "plain_text",
-              text: "Position"
-            },
-            element: {
-              action_id: "position",
-              type: "plain_text_input"
-            },
-            hint: {
-              type: "plain_text",
-              text: "What role would might they have?"
-            }
-          },
-          {
-            block_id: "email_block",
-            type: "input",
-            label: {
-              type: "plain_text",
-              text: "Email"
-            },
-            element: {
-              action_id: "email",
-              type: "plain_text_input"
-            },
-            hint: {
-              type: "plain_text",
-              text: "How can we contact them?"
-            }
-          },
-          {
-            block_id: "linkedin_block",
-            type: "input",
-            label: {
-              type: "plain_text",
-              text: "LinkedIn"
-            },
-            element: {
-              action_id: "linkedin",
-              type: "plain_text_input"
-            },
-            hint: {
-              type: "plain_text",
-              text: "Can you share their LinkedIn profile?"
-            }
-            // optional: true
-          }
-        ]
-      })
+          ]
+        })
+      };
+
+      // open the modal by calling views.open method and sending the payload
+      axios
+        .post(`${apiUrl}/views.open`, qs.stringify(view))
+        .then(result => {
+          debug("views.open: %o", result.data);
+          res.send("");
+        })
+        .catch(err => {
+          debug("views.open call failed: %o", err);
+          res.sendStatus(500);
+        });
     };
-
-    // open the dialog by calling dialogs.open method and sending the payload
-    axios
-      .post(`${apiUrl}/views.open`, qs.stringify(view))
-      .then(result => {
-        debug("views.open: %o", result.data);
-        res.send("");
-      })
-      .catch(err => {
-        debug("views.open call failed: %o", err);
-        res.sendStatus(500);
-      });
-    }
-
   } else {
     debug("Verification token mismatch");
     res.sendStatus(404);
@@ -429,7 +438,7 @@ app.post("/interactive", (req, res) => {
     // immediately respond with a empty 200 response to let
     // Slack know the command was received
     res.send("");
-    
+
     // create an employee object and set the userId property
     // to the Slack userId of the employee who submited the referral
     const employee = {};
@@ -447,20 +456,21 @@ app.post("/interactive", (req, res) => {
     referral.linkedin = values.linkedin_block.linkedin.value;
     referral.employee = body.user.name;
     referral.teamId = body.user.team_id;
-    
+
     // postInitMessage(referral);
-    getToken(team_id, req, res)
-    .then(record => postInitMessage(record, referral))
+    getToken(team_id, req, res).then(record =>
+      postInitMessage(record, referral)
+    );
 
     // look for the employee in the db, if it does not exist, create the employee, then (either way) create the referral record
     searchEmployee(employee)
       .then(record => pickRecord(record.employee))
       .then(record => createReferral(record, referral));
-    
-    // send confirmation direct message to employee who submitted the referral
-    getToken(team_id, req, res)
-    .then(record => confirmationmsg.triggerSend(body.view, body.user, record));
 
+    // send confirmation direct message to employee who submitted the referral
+    getToken(team_id, req, res).then(record =>
+      confirmationmsg.triggerSend(body.view, body.user, record)
+    );
   } else {
     debug("Token mismatch");
     res.sendStatus(404);
@@ -597,31 +607,93 @@ const createEmployee = employee => {
 // once the correct employee records are picked, it adds them to an array to be stringified
 // and posts to the Referral Leaderboard channel in Slack
 app.post("/zapier/ping", (req, res) => {
-  console.log('zapeier: ', req);
   const { token } = req.body;
   // check that the token matches expected value
   if (token === process.env.ZAPIER_TOKEN) {
     console.log("Zapier Webhook - Post Request");
     res.sendStatus(200);
 
-    listEmployees(token)
-      .then(employeeList => {
-        post(employeeList);
-      })
-      .catch(err => console.log(err));
+    listTeams().then(teamList => {
+      let teamArray = teamList.teamList;
+
+      // NEED TO PASS EACH TEAM OBJECT TO LISTEMPLOYEES, THEN NEED TO USE THE TEAM KEY IN LISTEMPLOYEES TO ACT AS THE IDENTIFIER AND
+      // CONTINUE TO PASS THE TOKEN THROUGH TO THE POST
+      teamArray.forEach(team => {
+        listEmployees(team)
+          .then(employeeList => {
+            post(employeeList);
+          })
+          .catch(err => console.log(err));
+      });
+    });
+
+    // WORKING CODE -> COMMENTED OUT TO TEST ABOVE
+    // listEmployees(token)
+    //   .then(employeeList => {
+    //     post(employeeList);
+    //   })
+    //   .catch(err => console.log(err));
   } else {
     res.sendStatus(500);
   }
 });
 
 // AIRTABLE
+// lists all teams in the Teams table
+// and pushes each teamId to an array called TeamList
+const listTeams = () => {
+  return new Promise((resolve, reject) => {
+    // create an empty array to push employee leaderboard objects to
+    const teamList = [];
+
+    base("Teams")
+      .select({
+        // Selecting all records in Grid view:
+        view: "Grid view"
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+
+          records.forEach(function(record) {
+            let teamIdRetrieved = record.get("TeamId");
+            let webhookUrl = record.get("webhook_url");
+
+            // employeeList not has an object for each employee that has submitted a referral
+            teamList.push({ [teamIdRetrieved]: webhookUrl });
+          });
+
+          resolve({
+            teamList: teamList
+          });
+
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+  });
+};
+
+// AIRTABLE
 // filters Airtable for employees with matching teamId, sorts by referralCount
 // and pushes each employee + their referralCount to an array called employeeList
-const listEmployees = teamId => {
+const listEmployees = teamObject => {
+  let teamId = Object.keys(teamObject)[0];
+  let webhookUrl = Object.values(teamObject)[0];
+
   return new Promise((resolve, reject) => {
     // create an empty array to push employee leaderboard objects to
     const employeeList = [];
     // filter for employees in the same Slack team as the employee who submitted the referral
+
     const employeeSearch = `TeamId = "${teamId}"`;
 
     base("Employees")
@@ -643,7 +715,8 @@ const listEmployees = teamId => {
           });
 
           resolve({
-            employeeList: employeeList
+            employeeList: employeeList,
+            webhookUrl: webhookUrl
           });
 
           // To fetch the next page of records, call `fetchNextPage`.
@@ -665,6 +738,7 @@ const listEmployees = teamId => {
 // posts the question to Slack
 const post = employeeList => {
   let employeeArray = employeeList.employeeList;
+  let webhookUrl = employeeList.webhookUrl;
 
   // define an empty array to push a block to for each employee on the leaderboard
   let blockArray = [];
@@ -682,7 +756,8 @@ const post = employeeList => {
     }
   });
 
-  let date = dateGenerator();
+  // let date = dateGenerator();
+  let date = moment().format("MMMM Do");
 
   // add a heading to the leaderboard post with the date
   blockArray.unshift(
@@ -706,7 +781,8 @@ const post = employeeList => {
   return new Promise((resolve, reject) => {
     request.post(
       {
-        url: process.env.SLACK_HOOK_URL,
+        // url: process.env.SLACK_HOOK_URL,
+        url: webhookUrl,
         json: json
       },
       (err, res) => {
@@ -719,18 +795,11 @@ const post = employeeList => {
   });
 };
 
-// Gets the current date and converts it into the month and daty
-const dateGenerator = () => {
-  let today = new Date();
-  let date = today.getMonth() + 1 + "/" + today.getDate();
-  return date;
-};
-
 // --------------------------------------------------------------------------------------------------------------------------
 // Post message via chat.postMessage
 const postInitMessage = (record, referral) => {
   let access_token = record.access_token;
-  
+
   let messageData = {
     channel: "#referral-engine",
     text:
@@ -740,32 +809,63 @@ const postInitMessage = (record, referral) => {
         type: "divider"
       },
       {
+        type: "context",
+        elements: [
+          {
+            type: "mrkdwn",
+            text: `This referral was submitted on ${moment().format(
+              "MMM Do YY"
+            )}`
+          }
+        ]
+      },
+      {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `You have a new referral submission! \n\n*Referrer:  <fakeLink.toEmployeeProfile.com|${referral.employee}>*`
+          text: `💥 *You have a new referral submission!* \n\n*Referrer:*  <google.com|${referral.employee}>`
         }
+      },
+      {
+        type: "divider"
       },
       {
         type: "section",
         fields: [
           {
             type: "mrkdwn",
-            text: `*📛 Name:* ${referral.name}`
+            text: `*Name* \n${referral.name}`
           },
           {
             type: "mrkdwn",
-            text: `*💼 Position:* ${referral.position}`
-          },
-          {
-            type: "mrkdwn",
-            text: `*✉️ Email:* ${referral.email}`
-          },
-          {
-            type: "mrkdwn",
-            text: `*🔗 LinkedIn:* ${referral.linkedin}`
+            text: `*Position* \n${referral.position}`
           }
         ]
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: `*Email* \n${referral.email}`
+          },
+          {
+            type: "mrkdwn",
+            text: `*LinkedIn* \n${referral.linkedin}`
+          }
+        ]
+      },
+      {
+        type: "context",
+        elements: [
+          {
+            type: "mrkdwn",
+            text: "Questions? Contact <support@refleaderboard.com>"
+          }
+        ]
+      },
+      {
+        type: "divider"
       }
     ]
   };
@@ -802,5 +902,4 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
-
-module.exports = { getToken }
+module.exports = { getToken };
